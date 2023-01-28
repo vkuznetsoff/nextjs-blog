8f0445a0def701cbb2175733590faa1bdd54ed40
Slavich <ss_slavich@mail.ru>
2023-01-28 17:25:33 +0300

    Created Layout and add CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout home={false}>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href={"/"}>Back home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
